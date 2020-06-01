e3f4b17be3b25ca9825fef2b65c9363d7e72d8d5
Pranjali Babshet (pbabshet) <pbabshet@cisco.com>
2020-06-01 16:32:08 +0530

    Add to existing

--- data/task1.py
+++ data/task1.py
@@ -1,25 +1,44 @@
 import json
 import xml.etree.ElementTree as ET 
 import yaml
+import unittest
+
+class Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(list,type(parseJSON('db.json')))
+        self.assertEqual(list,type(parseXML('db.xml')))
+        self.assertEqual(list,type(parseYML('db.yml')))
 
 def parseJSON(jsonfile):
+    out1 = []
     with open(jsonfile) as fj0:
         jdata = json.load(fj0)
     for key in jdata:
         print(key,":","paid :",jdata[key]["paid"],"due :",jdata[key]["due"])
+        out1.append((jdata[key]["paid"],jdata[key]["due"]))
+    return out1
 
 def parseXML(xmlfile):  
+    out2 = []
     tree = ET.parse(xmlfile) 
     root = tree.getroot() 
     for ele in root:
         print(ele.tag,":",ele[0].tag,":",ele[0].text,ele[1].tag,":",ele[1].text)
+        out2.append(([ele[0].text,ele[1].text]))
+    return out2
 
 def parseYML(ymlfile):
+    out3 = []
     with open(ymlfile) as file:
         documents = yaml.full_load(file)
     for item, doc in documents.items():
         print(item, ":","paid :",doc["paid"],"due :",doc["due"])
+        out3.append((doc["paid"],doc["due"]))
+    return out3
 
-parseJSON('db.json')
-parseXML('db.xml')
-parseYML('db.yml')
+x = Test()
+x.test()
+
+#parseJSON('db.json')
+#parseXML('db.xml')
+#parseYML('db.yml')
